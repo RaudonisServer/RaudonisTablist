a3d14555166e11d50292da2b51f3f92c76b2298c
R40fendt <r40test311@gmail.com>
2025-06-27 08:07:33 +0200

    PluginMsg-Klasse hinzugefügt

--- src/main/java/de/ritzenbergen/raudonistablist/RaudonisTablist.java
+++ src/main/java/de/ritzenbergen/raudonistablist/RaudonisTablist.java
@@ -1,6 +1,13 @@
 package de.ritzenbergen.raudonistablist;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.messaging.PluginMessageListener;
+
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+
 
 public final class RaudonisTablist extends JavaPlugin {
 
--- src/main/java/de/ritzenbergen/raudonistablist/pluginmsg/PluginMsg.java
+++ src/main/java/de/ritzenbergen/raudonistablist/pluginmsg/PluginMsg.java
@@ -1,4 +1,35 @@
 package de.ritzenbergen.raudonistablist.pluginmsg;
 
-public class PluginMsg {
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.messaging.PluginMessageListener;
+
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+
+public class PluginMsg implements PluginMessageListener {
+    @Override
+    public void onPluginMessageReceived(String channel, Player player, byte[] message) {
+        if (!channel.equals("my:tab_update")) return;
+
+        try (DataInputStream in = new DataInputStream(new ByteArrayInputStream(message))) {
+            String action = in.readUTF();
+            String name = in.readUTF();
+            String server = in.readUTF();
+
+            switch (action) {
+                case "join":
+                    break;
+                case "switch":
+
+                    break;
+                case "quit":
+
+                    break;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
